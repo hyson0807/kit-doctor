eae6c70a2355d4bbe370db0950a67283c979bcb9
hyson0807 <simsgood0807@gmail.com>
2025-05-26 19:51:03 +0900

    정보 입력부분 수정했음

--- app/(starter)/checkARC.tsx
+++ app/(starter)/checkARC.tsx
@@ -1,33 +1,27 @@
-import {View,Text, TouchableOpacity, TextInput} from 'react-native'
-import React, {useState} from 'react'
-import { Picker } from '@react-native-picker/picker';
 import BackBar from "@/components/BackBar";
-import {router} from "expo-router";
-import {t} from "i18next";
+import { api } from "@/convex/_generated/api";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import {useMutation} from "convex/react";
-import {api} from "@/convex/_generated/api";
+import { useMutation } from "convex/react";
+import { router } from "expo-router";
+import { t } from "i18next";
+import React, {useEffect, useRef, useState} from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 const CheckArc = () => {
     const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-    const years = Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i);
-    const [IsBorn, setIsBorn] = useState(false);
-    const [switched, setSwitched] = useState(true);
-    const [showNext, setShowNext] = useState(false);
+    const [showArc, setShowArc] = useState(true);
+    const [showInfo, setShowInfo] = useState(false);
     const [showNo, setShowNo] = useState(false);
-
 
     return (
                 <View className="flex-1 items-center">
                     <View className="flex flex-col items-center  w-full h-full sm:w-[640px] bg-background p-4 gap-10">
                         <BackBar />
-                        {switched && <Arc/>}
-                        {IsBorn && <Born/>}
+                        {showArc && <Arc/>}
+                        {showInfo && <Info/>}
                         {showNo && <IfNo/>}
-                        {showNext && <Next/>}
                     </View>
                 </View>
-
     )
 
     function Arc() {
@@ -38,9 +32,8 @@ const CheckArc = () => {
                 </View>
                 <View className="flex-row items-center justify-around w-full">
                     <TouchableOpacity onPress={() => {
-                        setSwitched(false);    // ARC 질문 숨김
                         setShowNo(false);
-                        setIsBorn(true);
+                        setShowInfo(true);
                     }}>
                         <View className="bg-buttonBlue w-[147px] h-[49px] rounded-3xl p-2 items-center justify-center">
                             <Text className="text-white text-[20.7px] font-bold">{t('yes')}</Text>
@@ -48,8 +41,7 @@ const CheckArc = () => {
                     </TouchableOpacity>
                     <TouchableOpacity onPress={() => {
                         setShowNo(true);
-
-
+                        setShowInfo(false);
                     }}>
                         <View className="bg-buttonBlue w-[147px] h-[49px] rounded-3xl p-2 items-center justify-center">
                             <Text className="text-white text-[20.7px] font-bold">{t('no')}</Text>
@@ -59,64 +51,60 @@ const CheckArc = () => {
             </View>
         )
     }
-
-    function Born() {
-
-
-        const handleYearChange = (value: number) => {
-            setSelectedYear(value);
-            setShowNext(true); // ✅ 연도 선택하면 다음 컴포넌트 보여주기
-        };
-
+    function Info() {
+        const [year, setYear] = useState('');
         const [name, setName] = useState('');
         const [email, setEmail] = useState('');
 
-        return (
-            <View className="flex items-center justify-center w-full p-4 rounded-3xl gap-4  ">
-                <View className="flex items-center justify-center p-4">
-                    <Text className="text-3xl text-center font-extrabold mb-10" >{t('select the year you born')}</Text>
-                </View>
-                <Picker
-                    style={{
-                        height: 60, // Increase height for mobile
-                        width: '80%', // Ensure it takes full width
-                        color: 'black',
-                        borderRadius: 10,
-                        marginBottom: 10,
-                        backgroundColor: "#F2F3F4"
+        function check() {
+            if(year.length === 4 && name.trim() !== "" && email.trim() !== "") {
+                router.push({
+                    pathname: '/myBenefit',
+                    params: { year: selectedYear.toString() }
+                })
+            } else {
+                alert("please fill all the fields!");
+            }
 
-                    }}
-                    selectedValue={selectedYear}
-                    onValueChange={handleYearChange}
-                    dropdownIconColor="black"
-                >
-                    {years.map((year) => (
-                        <Picker.Item key={year} label={`${year}`} value={year} />
-                    ))}
-                </Picker>
-                <View className="flex-1 items-center justify-center w-full h-10 ">
-                    <Text>당신을 위한 혜택이 있어요!</Text>
-                    <TextInput className="bg-primary" value={name} onChangeText={setName} placeholder={"Name"}/>
-                    <TextInput className="bg-primary"  value={email} onChangeText={setEmail} placeholder={"email"} />
+        }
+
+        return (
+            <View className="flex-1 items-center w-full">
+                <View className="flex items-center justify-center p-4 bg-primary rounded-3xl w-full gap-4">
+                        <Text className="text-3xl text-center flex-shrink font-extrabold">
+                            {t('select the year you born')}
+                        </Text>
+                        <TextInput
+                            value={year}
+                            onChangeText={setYear}
+                            placeholder="YYYY"
+                            placeholderTextColor="gray"
+                            maxLength={4}
+                            keyboardType="numeric"
+                            className="w-[80%] h-[60px] bg-white text-black text-xl rounded-2xl px-4"
+                        />
+                        <Text className="text-3xl text-center flex-shrink font-extrabold gap-4">당신을 위한 혜택이 있어요!</Text>
+                        <TextInput
+                        className="bg-white w-[80%] h-[60px] rounded-2xl p-2"
+                        value={name}
+                        onChangeText={setName}
+                        placeholder="Name"
+                        />
+                        <TextInput
+                        className="bg-white w-[80%] h-[60px] rounded-2xl p-2"
+                        value={email}
+                        onChangeText={setEmail}
+                        placeholder="Email"
+                        />
                 </View>
+                <TouchableOpacity className="absolute bottom-0 flex items-center justify-center w-[80%] mt-40 p-4 my-4  bg-buttonBlue rounded-2xl" onPress={check}>
+                    <Text className="text-white text-[20.7px] font-bold text-center">{t('check my benefit')}</Text>
+                </TouchableOpacity>
             </View>
         )
     }
 
-    function Next() {
-        return (
 
-                <TouchableOpacity
-                    className="flex items-center justify-center w-[80%] mt-40 p-4 my-4  bg-buttonBlue rounded-2xl"
-                    onPress={()=> router.push({
-                        pathname: '/myBenefit',
-                        params: { year: selectedYear.toString() }
-                    })}
-                >
-                    <Text className="text-white text-[20.7px] font-bold text-center">{t('check my benefit')}</Text>
-                </TouchableOpacity>
-        )
-    }
 
     function IfNo() {
         const getNumber = useMutation(api.users.getNumber);
